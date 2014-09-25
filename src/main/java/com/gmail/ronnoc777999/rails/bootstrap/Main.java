bd645611037661977bd0061a048379c555cac53e
RONNOC777 <ronnoc777999@gmail.com>
2014-09-25 17:31:52 +1200

    Added Rails Bootstrap To Git "Yay"
    This is only a reference and has no code as of yet!

---
+++ src/main/java/com/gmail/ronnoc777999/rails/bootstrap/Main.java
@@ -0,0 +1,13 @@
+package com.gmail.ronnoc777999.rails.bootstrap;
+
+public class Main {
+	
+	/*
+	 *             - = Project Rails Bootstrap = -
+	 *             
+	 *            We will start developing this soon!
+	 *            
+	 *            
+	 */
+
+}
